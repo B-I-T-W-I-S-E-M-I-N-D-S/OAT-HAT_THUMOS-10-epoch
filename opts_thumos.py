8b0aa14e09e2e66abf042da44185bcf05d9738f8
Mohammed Aman Bhuiyan <107806297+Aman554-EQ@users.noreply.github.com>
2025-07-15 02:09:59 +0600

    Update opts_thumos.py

--- opts_thumos.py
+++ opts_thumos.py
@@ -120,7 +120,7 @@ def parse_opt():
     parser.add_argument(
         '--lr_step',
         type=int,
-        default=4)
+        default=3)
         
     # Post processing
     parser.add_argument(
